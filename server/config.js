4815c973ec0dde0e3d64ec7838566b8de4a30a90
Zachary Silver <zmansilver@gmail.com>
2021-02-19 16:19:05 -0800

    initial commit

---
+++ server/config.js
@@ -0,0 +1,15 @@
+const TWITCH_CLIENT_ID = '';
+const TWITCH_SECRET    = '';
+const SESSION_SECRET   = '';
+const CALLBACK_URL     = '';
+const DB               = '';
+const PORT             = process.env.PORT || 5000;
+
+module.exports = {
+  TWITCH_CLIENT_ID,
+  TWITCH_SECRET,
+  SESSION_SECRET,
+  CALLBACK_URL,
+  DB,
+  PORT
+};

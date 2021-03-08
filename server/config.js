2b294b8c6f033f40148e31f06ff3abf5f2490794
Zachary Silver <zmansilver@gmail.com>
2021-03-07 22:10:12 -0800

    fixed package.json and updated config

--- server/config.js
+++ server/config.js
@@ -1,11 +1,14 @@
-const TWITCH_API_URL   = 'https://api.twitch.tv/helix';
-const TWITCH_AUTH_URL  = 'https://id.twitch.tv/oauth2/authorize';
-const TWITCH_TOKEN_URL = 'https://id.twitch.tv/oauth2/token';
-const TWITCH_CLIENT_ID = '';
-const TWITCH_SECRET    = '';
-const SESSION_SECRET   = '';
-const CALLBACK_URL     = '';
-const PORT             = process.env.PORT || 5000;
+const TWITCH_API_URL           = 'https://api.twitch.tv/helix';
+const TWITCH_AUTH_URL          = 'https://id.twitch.tv/oauth2/authorize';
+const TWITCH_TOKEN_URL         = 'https://id.twitch.tv/oauth2/token';
+const TWITCH_CLIENT_ID         = '';
+const TWITCH_SECRET            = '';
+const TWITCH_IRC_TOKEN         = '';
+const TWITCH_CHATBOT_CLIENT_ID = '';
+const TWITCH_CHATBOT_SECRET    = '';
+const SESSION_SECRET           = '';
+const CALLBACK_URL             = '';
+const PORT                     = process.env.PORT || 5000;
 
 module.exports = {
   TWITCH_API_URL,
@@ -13,6 +16,9 @@ module.exports = {
   TWITCH_TOKEN_URL,
   TWITCH_CLIENT_ID,
   TWITCH_SECRET,
+  TWITCH_IRC_TOKEN,
+  TWITCH_CHATBOT_CLIENT_ID,
+  TWITCH_CHATBOT_SECRET,
   SESSION_SECRET,
   CALLBACK_URL,
   PORT

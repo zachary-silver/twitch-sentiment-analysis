28677f38db9a5806699d63d98999efcbe7fd8e0f
Zachary Silver <zmansilver@gmail.com>
2021-05-19 12:28:16 -0700

    updated flask app launch

--- server/flask/api.py
+++ server/flask/api.py
@@ -8,10 +8,6 @@ analyze.setup('./analysis')
 MODEL = analyze.load_model('./analysis/models/rf_with_emotes')
 
 
-def get_sentiment_label(s):
-    return 'positive' if s == 1 else 'negative' if s == -1 else 'neutral'
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
     sentiments = analyze.predict_messages([message['content']
@@ -23,7 +19,3 @@ def predict():
 @app.route('/report', methods=['GET'])
 def report():
     return analyze.get_classification_report(MODEL, './analysis')
-
-
-if __name__ == '__main__':
-    app.run(host='localhost', port=8080, debug=True)
